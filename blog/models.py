40173d8726b688110014045cf9086e40052258a9
RouSage <therousage@gmail.com>
2018-09-12 16:13:42 +0300

    Indexes for models' fields

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=250)
-    url_slug = models.CharField(max_length=250)
+    url_slug = models.CharField(db_index=True, max_length=250)
     created_at = models.DateTimeField(verbose_name="created at", default=timezone.now())
 
     def __str__(self):
@@ -14,9 +14,9 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=5000)
     content = models.TextField()
-    url_slug = models.CharField(max_length=250)
+    url_slug = models.CharField(db_index=True, max_length=250)
     published = models.BooleanField(default=False)
-    posted_on = models.DateTimeField()
+    posted_on = models.DateTimeField(db_index=True)
     # Category FK
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 

5d3abffa594fdacdbb641ce1ea1e337ba6ba055b
RouSage <therousage@gmail.com>
2018-09-12 17:21:11 +0300

    Basic Index and Detail views

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = "blog"
 urlpatterns = [
-    path("", views.index, name="index")
+    # ex: /blog/
+    path("", views.index, name="index"),
+    # ex: /blog/lorem_ipsum
+    path("<slug:post_slug>/", views.detail, name="detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_posts = Post.objects.filter(published=True).order_by("-posted_on")[:10]
+    context = {
+        "latest_posts": latest_posts,
+    }
+    return render(request, "blog/index.html", context)
+
+def detail(request, post_slug):
+    post = get_object_or_404(Post, url_slug=post_slug)
+    return render(request, "blog/detail.html", {'post': post})

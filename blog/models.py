9cb9b9fce423214ad1870487911552a4b93e0f2c
RouSage <therousage@gmail.com>
2018-09-23 17:36:10 +0300

    Added SlugField for slugs insead of CharField

--- blog/models.py
+++ blog/models.py
@@ -4,21 +4,23 @@ from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=250)
-    url_slug = models.CharField(db_index=True, max_length=250)
-    created_at = models.DateTimeField(verbose_name="created at", default=timezone.now())
+    url_slug = models.SlugField(unique=True, db_index=True, max_length=50)
+    created_at = models.DateTimeField(
+        verbose_name="created at", default=timezone.now())
 
     def __str__(self):
-            return self.name
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField(max_length=5000)
     content = models.TextField()
-    url_slug = models.CharField(db_index=True, max_length=250)
+    url_slug = models.SlugField(unique=True, db_index=True, max_length=50)
     published = models.BooleanField(default=False)
     posted_on = models.DateTimeField(db_index=True)
     # Category FK
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
-            return self.title
+        return self.title

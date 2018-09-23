fe90ebcfa041d3a2f8804b6aa1a8ea768035cb86
RouSage <therousage@gmail.com>
2018-09-23 17:06:38 +0300

    Dynamic page title

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Post
 
 
 def index(request):
-    posts = Post.objects.filter(published=True).order_by("-posted_on")[:10]
+    posts = get_list_or_404(Post.objects.order_by("-posted_on"), published=True)[:10]
     context = {
         "posts": posts,
+        "page_title": "Latest posts"
     }
     return render(request, "blog/index.html", context)
 
 def archive(request, year, month):
-    posts = Post.objects.filter(posted_on__year=year, posted_on__month=month)
+    posts = get_list_or_404(Post.objects.oder_by("-posted_on"), published=True,
+                            posted_on__year=year, posted_on__month=month)[:10]
     context = {
-        "posts": posts
+        "posts": posts,
+        "page_title": "Posts by {} year and {}th month".format(year, month)
     }
     return render(request, "blog/index.html", context)
 
 
 def detail(request, post_slug):
     post = get_object_or_404(Post, url_slug=post_slug)
-    return render(request, "blog/detail.html", {'post': post})
+    context = {
+        "post": post
+    }
+    return render(request, "blog/detail.html", context)

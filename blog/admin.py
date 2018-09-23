2d3023fec3547d92698edf6502ce0c4ceaf12250
RouSage <therousage@gmail.com>
2018-09-23 19:27:16 +0300

    Category customization for admin page

--- blog/admin.py
+++ blog/admin.py
@@ -15,5 +15,15 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['posted_on', 'published', 'category']
     search_fields = ['title']
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": [
+            "name", "created_at"
+        ]})
+    ]
+    list_display = ('name', 'url_slug', 'created_at')
+    list_filter = ['created_at']
+    search_fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)

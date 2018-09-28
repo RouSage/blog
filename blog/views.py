f04aa106251689041ad3df21a3db8b231d3ad094
RouSage <therousage@gmail.com>
2018-09-28 14:02:18 +0300

    Some customization

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Post, Category, Tag
-from django.db.models import Count
 
 
 def index(request):
@@ -26,19 +25,23 @@ def archive_date(request, year, month):
 
 
 def category(request, category_slug):
-    category = get_object_or_404(Category, url_slug=category_slug)
+    category_by_slug = get_object_or_404(Category, url_slug=category_slug)
     context = {
-        "posts": category.post_set.all(),
-        "page_title": "Posts by '{}' category".format(category.name)
+        "posts": category_by_slug.post_set.filter(published=True).order_by("-posted_on"),
+        "page_title": "Posts by '{}' category".format(category_by_slug.name),
+        "categories": get_categories(),
+        "tags": get_tags()
     }
     return render(request, "blog/index.html", context)
 
 
 def tag(request, tag_slug):
-    tag = get_object_or_404(Tag, url_slug=tag_slug)
+    tag_by_slug = get_object_or_404(Tag, url_slug=tag_slug)
     context = {
-        "posts": tag.post_set.all(),
-        "page_tile": "Posts by '{}' tag".format(tag.name)
+        "posts": tag_by_slug.post_set.filter(published=True).order_by("-posted_on"),
+        "page_title": "Posts by '{}' tag".format(tag_by_slug.name),
+        "categories": get_categories(),
+        "tags": get_tags()
     }
     return render(request, "blog/index.html", context)
 
@@ -46,7 +49,9 @@ def tag(request, tag_slug):
 def detail(request, post_slug):
     post = get_object_or_404(Post, url_slug=post_slug)
     context = {
-        "post": post
+        "post": post,
+        "categories": get_categories(),
+        "tags": get_tags()
     }
     return render(request, "blog/detail.html", context)
 

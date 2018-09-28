c73633982dfed9fefb8a199e1cd6a51b109aaa9f
RouSage <therousage@gmail.com>
2018-09-28 12:58:42 +0300

    Show posts and categories in sidebar

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("<slug:post_slug>/", views.detail, name="detail"),
     # ex: /blog/archive/2018/10
     path("archive/<int:year>/<int:month>/", views.archive_date, name="archive_date"),
-    # ex: /blog/programming
-    path("archive/<slug:category_slug>/", views.archive_category, name="archive_category")
+    # ex: /blog/archive/programming
+    path("categories/<slug:category_slug>/", views.category, name="category"),
+    # ex: /blog/archive/web
+    path("tags/<slug:tag_slug>/", views.tag, name="tag")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import Post, Category
+from .models import Post, Category, Tag
+from django.db.models import Count
 
 
 def index(request):
     posts = get_list_or_404(Post.objects.order_by(
         "-posted_on"), published=True)[:10]
+    categories = Category.objects.annotate(
+        n_posts=Count('post')).order_by('name')
+    tags = Tag.objects.all().order_by('name')
     context = {
         "posts": posts,
-        "page_title": "Latest posts"
+        "page_title": "Latest posts",
+        "categories": categories,
+        "tags": tags
     }
     return render(request, "blog/index.html", context)
 
@@ -22,11 +28,20 @@ def archive_date(request, year, month):
     return render(request, "blog/index.html", context)
 
 
-def archive_category(request, category_slug):
+def category(request, category_slug):
     category = get_object_or_404(Category, url_slug=category_slug)
     context = {
         "posts": category.post_set.all(),
         "page_title": "Posts by '{}' category".format(category.name)
+    }
+    return render(request, "blog/index.html", context)
+
+
+def tag(request, tag_slug):
+    tag = get_object_or_404(Tag, url_slug=tag_slug)
+    context = {
+        "posts": tag.post_set.all(),
+        "page_tile": "Posts by '{}' tag".format(tag.name)
     }
     return render(request, "blog/index.html", context)
 

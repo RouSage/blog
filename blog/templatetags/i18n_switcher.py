1cd89f8ee93bab58ee705856f47d6f4eea2a915d
RouSage <therousage@gmail.com>
2019-06-16 18:34:36 +0300

    Register templatetags as a package

--- blog/templatetags/i18n_switcher.py
+++ blog/templatetags/i18n_switcher.py
@@ -6,12 +6,12 @@ from blog.utils import switch_language
 register = template.Library()
 
 
-@register.filter
+@register.filter(name='switch_i18n_prefix')
 @stringfilter
 def switch_i18n_prefix(path, language):
     return switch_language(path, language)
 
 
-@register.filter
+@register.filter(name='switch_i18n')
 def switch_i18n(request, language):
     return switch_language(request.get_full_path(), language)
